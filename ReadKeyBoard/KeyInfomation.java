bf0454a795bd1d97be6cd1f34ceef3a1e351d2e0
Asterisk <kuro5858tube@gmail.com>
2017-05-28 22:45:42 +0900

    #2 Added Some Core Files. (Initialize commit)
    #2 Added Some Core Files. (Initialize commit)

---
+++ ReadKeyBoard/KeyInfomation.java
@@ -0,0 +1,64 @@
+import java.applet.Applet;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/// <summary>
+/// キー情報の管理クラス
+/// </summary>
+public class KeyInformation extends Applet{
+    private String   _TxStr;
+    private float    _width;
+    private float    _height;
+
+    private KeyInformation _key;
+    private FontMetrics _f;
+    //private BufferedReader _reader;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    private KeyInformation( float width, float height ) {
+        this._width       = width;
+        this._height      = height;
+        this._TxStr       = "";
+        //this._reader      = new BufferedReader( new InputStreamReader( System.in ) );
+     }
+
+    /// <summary>
+    /// Singleton パターンに対応。
+    /// </summary>
+    private KeyInformation getInstance(){
+        return ( this._key );
+    }
+
+    /// <summary>
+    /// paint()
+    /// Argument : Graphics g
+    /// </summary>
+    public void paint(Graphics g) {
+        Initialize( );
+		g.drawString( _TxStr , (width / 2) - (_f.stringWidth(_TxStr)) / 2 , (height / 2) );
+    }
+    
+    /// <summary>
+    /// Initalize( )
+    /// Argument : Nothing
+    /// </summary>
+    public void Initialize(){
+        try {
+            addKeyListener(
+                new KeyAdapter() {
+			        public void keyPressed(final KeyEvent e){
+                         _TxStr = e.getKeyText(e.getKeyCode());
+                        repaint();
+			        }
+		        }
+            );
+            System.out.println( "String is : " + this._TxStr );
+        } 
+        catch ( Exception e ) {
+            System.out.println( "Exception :" + e );
+        }
+    }
+}
---
+++ ReadKeyBoard/Main.java
@@ -0,0 +1,12 @@
+/// <summary>
+/// Main クラス
+/// </summary>
+public class Main {
+    public static final int main( String[] args ){
+        private static final float _width  = 500;
+        private static final float _height = 300;
+
+        KeyInformation _key = new KeyInformation(_width, _height);
+        _key.paint();
+    }
+}
